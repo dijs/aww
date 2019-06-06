3722574aee7528d6c4e3b3abc54ab74fa23ce428
Richard van der Dys <richard.vanderdys@gmail.com>
2019-06-06 12:09:07 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function usePosts() {
+  const [posts, setPosts] = React.useState([]);
+  if (!posts.length) {
+    fetch('https://api.reddit.com/r/aww/new.json')
+      .then(res => res.json())
+      .then(data => {
+        setPosts(
+          data.data.children.map(child => ({
+            title: child.data.title,
+            thumbnail: child.data.thumbnail,
+            embed: child.data.embed
+          }))
+        );
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  return { posts };
+}
+
+function Post({ title, thumbnail }) {
+  return (
+    <article key={title}>
+      <h2>{title}</h2>
+      <img src={thumbnail} alt={title} />
+    </article>
+  );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { posts } = usePosts();
+  return <div className="container">{posts.map(Post)}</div>;
 }
 
 export default App;
